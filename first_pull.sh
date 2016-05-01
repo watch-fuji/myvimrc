ec08424b6a5d7256ad26ad1293a7067bb250db0c
aokiyuya <yuya.aoki@fujielab.org>
2016-05-02 00:21:46 +0900

    fix first_pull.sh

--- first_pull.sh
+++ first_pull.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd ~/
+sh myvimrc/pull_vimrc.sh
+rm -rf .vim/
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
-cd myvimrc/
-sh pull_vimrc.sh
-cd ~/
 vim
